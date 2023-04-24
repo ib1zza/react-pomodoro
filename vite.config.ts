e41fb4666fdbe716f1768d5d45cecc6068181008
ib1zza <dremast1337@gmail.com>
2023-04-24 08:48:33 +0300

    pwa first try

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,48 @@
 import path from "path";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
+const vitePwa = VitePWA({
+  registerType: "autoUpdate",
+  outDir: "dist",
+  manifest: {
+    name: "Pomodoro",
+    short_name: "Pomodoro",
+    theme_color: "#ffffff",
+    description: "Pomodoro timer application",
+    icons: [
+      {
+        src: "assets/icons/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "assets/icons/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "assets/icons/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@templates': path.resolve(__dirname, './src/templates'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@assets': path.resolve(__dirname, './src/assets'),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@templates": path.resolve(__dirname, "./src/templates"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
-
   },
-  plugins: [react()]
-})
+  plugins: [react(), vitePwa],
+});
---
+++ workbox-config.ts
@@ -0,0 +1,5 @@
+export default {
+  globalDirectory: "build/",
+  globPatterns: ["**/*.{js,jsx,ts,tsx, css,woff2,png,svg,jpg,jpeg,}"],
+  swDest: "build/sw.js",
+};
